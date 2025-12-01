c5ac57c9663a8c2b866f55728373f74e73e55a3d
Xander Nguyen <xander.nguyen313@gmail.com>
2025-11-30 22:37:54 -0600

    first push

---
+++ src/main/java/com/project/demo/DemoController.java
@@ -0,0 +1,13 @@
+package com.project.demo;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class DemoController {
+
+    @GetMapping("/hello")
+    public String hello(){
+        return "Hello World";
+    }
+}
